f10ebba5a2aa033e1cc8b0251258f8eb0f01cf91
Himanshu Saini <himanshu.saini.cse@gmail.com>
2026-03-17 20:45:54 +0530

    lec 4

--- javabasics.java
+++ javabasics.java
@@ -69,28 +69,49 @@
 
 //                                             lecture 3
 //                                                       Arrays
-public class javabasics {
+// public class javabasics {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
        
-        int arr[] = new int[5];// the number of slots in the bucket
+//         int arr[] = new int[5];// the number of slots in the bucket
 
-        // Aeeay = very big bucket , having multiple slots
+//         // Aeeay = very big bucket , having multiple slots
 
-        //slot1, slot2, slot3, slot4, slot5
-        //counting starts from
-        //0, 1, 2, 3, 4,     == index of the array
+//         //slot1, slot2, slot3, slot4, slot5
+//         //counting starts from
+//         //0, 1, 2, 3, 4,     == index of the array
 
-        arr[0] = 10;//in slot1 we are putting 10
-        arr[1] = 20;//in slot2 we are putting 20 
-        arr[2] = 30;//in slot3 we are putting 30
-        arr[3] = 40;//in slot4 we are putting 40
+//         arr[0] = 10;//in slot1 we are putting 10
+//         arr[1] = 20;//in slot2 we are putting 20 
+//         arr[2] = 30;//in slot3 we are putting 30
+//         arr[3] = 40;//in slot4 we are putting 40
 
-        System.out.println(arr[0]);//it will print the value of slot1 which is 10
-            System.out.println(arr[1]);//it will print the value of slot2 which is 20
-            System.out.println(arr[2]);//it will print the value of slot3 which is 30
-            System.out.println(arr[3]);//it will print the value of slot4 which is 40
-            System.out.println(arr[4]);//it will print the value of slot5 which is 0 because we have not put any value in it and by default it takes 0 for int data type
+//         System.out.println(arr[0]);//it will print the value of slot1 which is 10
+//             System.out.println(arr[1]);//it will print the value of slot2 which is 20
+//             System.out.println(arr[2]);//it will print the value of slot3 which is 30
+//             System.out.println(arr[3]);//it will print the value of slot4 which is 40
+//             System.out.println(arr[4]);//it will print the value of slot5 which is 0 because we have not put any value in it and by default it takes 0 for int data type
 
-    }
-}
+
+//             int arr1[] = new int[]{10, 20, 30, 40, 50};// we can also initialize the array like this
+//                 System.out.println(arr1[0]);//it will print the value of slot1 which is 10
+//                 System.out.println(arr1[1]);//it will print the value of slot2 which is 20
+
+//                 String arr2[] = new String[4];
+//                 // integer arrays are initialized with 0 by default but string arrays are initialized with null by default because string is a reference data type
+                
+//                 arr2[0] = "Himanshu";
+//                 arr2[1] = "Saini";
+//                 //arr2[2] = "10"; not allowed because arr2 is a string array and we are trying to put an integer value in it
+//                 System.out.println(arr2[0]);//it will print the value of slot1 which is "Himanshu"
+
+//     }
+// }
+
+
+
+//                                             lecture 4
+//                                                       if else statementf
+
+
+
